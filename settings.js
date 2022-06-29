f22190a78cb18c899a7a8aeb16e9d198500af734
YuzakiTodz <97385500+YuzkiTodz@users.noreply.github.com>
2022-06-30 01:08:04 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -31,8 +31,8 @@ global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder i
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://i.postimg.cc/L84bLpxL/menu-1.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.owner = ["62895638102488","62895638102488"] //ganti agar fitur owner bisa di gunakan
-global.packname = 'Agus' //sticker wm ubah
-global.author = 'bin Yanto' //sticker wm ganti nama kalian
+global.packname = 'Tsukasa' //sticker wm ubah
+global.author = 'Istrinya Arul' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
